32084a4686ff09a779460ba42c5f51a103540d74
evanreynolds9 <evanreynolds9@gmail.com>
2025-08-31 20:44:16 -0400

    Began testing pipeline for edgartools

--- etl/Notebooks/etl_utils.py
+++ etl/Notebooks/etl_utils.py
@@ -4,25 +4,29 @@ import pandas as pd
 import os
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.exc import DataError
+from typing import Optional, Union, Dict, Sequence, Any
 
 
 # extract_query: fetch the results of a SQL query at a given file path and return the results in a pandas dataframe
 # Inputs:
 # db_conn_str: str - pymysql connection string
 # sql_query_path: str - path to the SQL query to be executed and returned
+# params: optional dict or sequence - parameters to be passed into the sql query
 # Outputs:
 # df: pd.DataFrame - data frame containing the results of the query
-def extract_query(db_conn_str: str, sql_query_path: str) -> pd.DataFrame:
+def extract_query(db_conn_str: str,
+                  sql_query: str,
+                  params: Optional[Union[Dict[str, Any], Sequence[Any]]] = None
+) -> pd.DataFrame:
     try:
-        with open(sql_query_path, "r") as file:
-            sql_query = file.read()
-
         # Create sqlalchemy engine
         engine = create_engine(db_conn_str)
+
+        # Run query
         with engine.connect() as conn:
             print("Connection Successful!")
-            df = pd.read_sql(sql_query, conn)
-            print(f"SQL script at {os.path.basename(sql_query_path)} executed successfully!")
+            df = pd.read_sql(sql_query, conn, params = params)
+            print(f"SQL script executed successfully!")
 
     except Exception as e:
         print(f"Error during connection or execution of query: {e}")
--- etl/SQL/Create_DB_Tables.sql
+++ etl/SQL/Create_DB_Tables.sql
@@ -4,7 +4,7 @@ CREATE DATABASE stocks_etl_ml_db;
 USE stocks_etl_ml_db;
 
 -- Create table used to store S&P tickers
-CREATE TABLE SP_Constituents (
+CREATE TABLE sp_constituents (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     Actual_Date DATETIME,
     Load_Date DATETIME,
